cb98fc76dee72c1722113cd83131b35edba7b18b
Felipe Baltor <fbaltor@gmail.com>
2021-07-13 17:37:01 -0300

    Containerization done!

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT ? +process.env.SERVER_PORT : 3000;
 
 app.get('/', (req, res) => {
   res.send('Root endpoint working!');
 });
 
 app.listen(PORT, () => {
-  console.log(`[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
